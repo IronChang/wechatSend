8681025632a6f139940acf824800991725f44976
IronChang <48543011+IronChang@users.noreply.github.com>
2025-03-27 16:48:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -441,9 +441,8 @@ weather, max_temperature, min_temperature, now_weather, wind_direction, air_humi
 # 获取词霸每日金句
 note_ch, note_en = get_ciba()
 caihongpi = caihongpi()
-note_ch1, note_en2 = split_caihong_text(caihongpi)
 # 公众号推送消息
 for user in users:
-    send_message(user, accessToken, city, weather, max_temperature, min_temperature, note_ch1, note_en2, now_weather,
-                 wind_direction, air_humidity, ultraviolet_rays, air_quality, pm, sunrise, sunset, greetings_today)
+    send_message(user, accessToken, city, weather, max_temperature, min_temperature, note_ch, note_en, now_weather,
+                 wind_direction, air_humidity, ultraviolet_rays, air_quality, pm, sunrise, sunset, caihongpi)
 os.system("pause")
