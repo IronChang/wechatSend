4daff13b544bfda0678fc986cea23f80d998acfc
IronChang <48543011+IronChang@users.noreply.github.com>
2025-03-27 15:05:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -220,7 +220,8 @@ def caihongpi():
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
     }
     response = get(url,headers=headers)
-    res = response.json()["data"]["text"]
+    print("Raw response:", response.text[:100]) 
+    res = response.json()["data"]
     return res
 
 def get_ciba():
